6d2a7e054a1430f9d893934323ec626e2c551283
Matthew Harwood <matthhar12@gmail.com>
2019-02-17 20:06:04 -0800

    Added server side rendering

---
+++ core/src/main.ts
@@ -0,0 +1,22 @@
+import { NestFactory, FastifyAdapter } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { join } from 'path';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, new FastifyAdapter());
+  
+  app.useStaticAssets({
+    root: join(__dirname, '..', 'public'),
+    prefix: '/public/',
+  });
+
+  app.setViewEngine({
+    engine: {
+      handlebars: require('handlebars'),
+    },
+    templates: join(__dirname, '..', 'views'),
+  });
+
+  await app.listen(6969);
+}
+bootstrap();
